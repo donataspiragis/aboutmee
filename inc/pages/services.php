cb4701e4d7925bc0be752d3ab3ab814d341cb18b
Donatas Piragis <donatas.piragis@gmail.com>
2019-09-12 17:40:27 +0300

    Services hexagons fix #5

--- inc/pages/services.php
+++ inc/pages/services.php
@@ -8,7 +8,7 @@
 <div class="card">
     <i class="far fa-lightbulb"></i>
     <h3>Web design</h3>
-    <p>I can make create design for your website</p>
+    <p>I can create design for your website</p>
 </div>
 <div class="card">
     <i class="fab fa-android"></i>
@@ -23,25 +23,34 @@
 </div>
 <div class="container flex-fix">
     <h1 id="services">MY SERVICES ARE</h1>
+    <div class="bar-bottom bcm-black"></div>
 </div>
 <div class="container">
-    <div>
-    <div class="hexagon"><span><i class="fas fa-tachometer-alt"></i></span></div>
+    <div class="hexa-cont">
+        <div class="hexator">
+            <div class="hexagon"><span><i class="fas fa-tachometer-alt"></i></span></div>
+        </div>
     <h5>Fast</h5>
     <p>Fast load times and lag free interaction, my highest priority.</p>
     </div>
-    <div>
-    <div class="hexagon"><span><i class="fas fa-desktop"></i></span></div>
+    <div class="hexa-cont">
+        <div class="hexator">
+            <div class="hexagon"><span><i class="fas fa-desktop"></i></span></div>
+        </div>
     <h5>Responsive</h5>
     <p>My layouts will work on any device, big or small.</p>
     </div>
-    <div>
-    <div class="hexagon"><span><i class="far fa-question-circle"></i></span></div>
+    <div class="hexa-cont">
+        <div class="hexator">
+            <div class="hexagon"><span><i class="far fa-question-circle"></i></span></div>
+        </div>
     <h5>Intuitive</h5>
     <p>Strong preference for easy to use, intuitive UX/UI.</p>
     </div>
-    <div>
-    <div class="hexagon"><span><i class="fas fa-dice"></i></span></div>
+    <div class="hexa-cont">
+        <div class="hexator">
+            <div class="hexagon"><span><i class="fas fa-dice"></i></span></div>
+        </div>
     <h5>Dynamic</h5>
     <p>Websites don't have to be static, I love making pages come to life.</p>
     </div>
