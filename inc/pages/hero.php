9f4bd7de9914c58af4bd57d72fe7972e3078874a
Donatas Piragis <donatas.piragis@gmail.com>
2019-09-12 12:57:17 +0300

    Hero section final update #16

--- inc/pages/hero.php
+++ inc/pages/hero.php
@@ -3,8 +3,7 @@
 
     <div class="hero-content">
         <h1> 	&#60;<span class="highlight">Pie</span>Code&#62; </h1>
-        <p>You dream about website ?</p>
-        <p>I will make it for you as easy as cooking a Pie!</p>
-        <a href="#contact" class="btn btn-form">Contact Me</a>
+        <p>You dream about <strong style="letter-spacing: 1px; text-transform: uppercase;"><span class="highlight">Web</span>site</strong>  ?</p>
+        <a href="#contact" class="btn btn-form btn-animated">HIRE ME</a>
     </div>
 </div>
