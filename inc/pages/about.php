f3020049bbee32b66a5e68b97002d95451309723
Donatas Piragis <donatas.piragis@gmail.com>
2019-09-12 15:36:33 +0300

    About me section design update #7

--- inc/pages/about.php
+++ inc/pages/about.php
@@ -1,18 +1,28 @@
-<section class="section-margin grey-background section-bigger">
-
+<section class="section-margin dark-grey section-bigger about-me">
 <div class="container">
     <div class="colum-1">
+        <div class="image-container">
         <img src="images/me.jpg" alt="my photo">
+        </div>
     </div>
     <div class="colum-2">
     <h1 id="about">about me</h1>
+    <div class="bar-bottom bcm-red"></div>
         <p>
         I'm the Front-End Developer and I would like to make something special for you.
+        </p><p>
         I believe that it is possible to do everything just it is a must to put effort, 
         be flexible and not to be afraid sometimes to make mistake and learn from it.
         </p>
-        <a href="#history" class="btn btn-form">My history</a>
-        <a href="#contact" class="btn btn-form">Contact Me</a>
+        <p>
+            I enjoy travelling and catching new ideas during my travels, so I would always 
+            put fresh ideas in my work and that there would be no repeating stuff.
+        </p>
+        <p>
+            More about me you will know when we will start working together, so <a href="#contact"><span class="highlight-2"> let's start working!</span></a>
+        </p>
+        <a href="#history" class="btn-small btn-form btn-g">History</a>
+        <a href="#contact" class="btn-small btn-form">Contact Me</a>
     </div>
 </div>
 
