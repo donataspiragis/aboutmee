a0df4c9780b38cbce5129963c764f33f8fe047d1
Donatas Piragis <donatas.piragis@gmail.com>
2019-09-12 13:41:58 +0300

    Footer icons #13

--- inc/footer.php
+++ inc/footer.php
@@ -4,17 +4,17 @@
     <div class="container">
         <div class="foot-content">
             <div class="icons-cont">
-                <a href="" >
+                <a href="https://www.linkedin.com/in/donatas-piragis-66227164/" target="_blank" >
                     <div class="icon">
                     <i class="fab fa-linkedin-in okk"></i>
                     </div>
                 </a>
-                <a href="" class="icon">
+                <a href="https://www.facebook.com/donatas.piragis?ref=bookmarks" target="_blank" class="icon">
                     <div class="icon">
                     <i class="fab fa-facebook-f okk"></i>
                     </div>
                 </a>
-                <a href="" class="icon">
+                <a href="https://www.instagram.com/donatas_piragis/" target="_blank" class="icon">
                     <div class="icon">
                     <i class="fab fa-instagram okk"></i>
                     </div>
