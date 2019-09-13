d15f15947d094355d472cb3febe46edad5924df3
Donatas Piragis <donatas.piragis@gmail.com>
2019-09-13 18:12:47 +0300

    Skills section design update #8

--- inc/pages/skills.php
+++ inc/pages/skills.php
@@ -3,21 +3,39 @@
     <h1>SKILLS</h1>
 </div>
 <div class="container">
-    <div class="card bcm-white">
-        <img src="images/new-php-logo.png" alt="">
+<div class="bar-bottom bcm-black"></div>
+</div>
+<div class="container">
+    <div class="card-skill bcm-white">
+        <div class="img-cont">
+            <img src="images/new-php-logo.png" alt="">
+        </div>
         <h5>PHP</h5>
     </div>
-    <div class="card bcm-white">
-    <img src="images/Laravel_logo.png" alt="">
+    <div class="card-skill bcm-white">
+    <div class="img-cont">
+        <img src="images/Laravel_logo.png" alt="" style="margin-top: 45px;">
+    </div>
         <h5>Laravel</h5>
     </div>
-    <div class="card bcm-white">
-    <img src="images/htcsjs.png" alt="">
-    
-        <h5> HTML, CSS, JS</h5>
-   
-
-</div>
+    <div class="card-skill bcm-white">
+    <div class="img-cont">
+        <img src="images/html.png" alt="">
+    </div>
+        <h5>HTML</h5>
+    </div>
+    <div class="card-skill bcm-white">
+    <div class="img-cont">
+        <img src="images/css.png" alt="">
+    </div>
+        <h5>CSS</h5>
+    </div>
+    <div class="card-skill bcm-white">
+    <div class="img-cont">
+        <img src="images/javascript.png" alt="">
+    </div>
+        <h5>Javascript</h5>
+    </div>
 </div>
 
 </section>
